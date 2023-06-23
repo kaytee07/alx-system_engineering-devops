d783daff00cb840f82a909f3d86ecaa98ece8033
kaytee07 <kofi.taylor07@gmail.com>
2023-06-23 18:14:37 +0000

    exec kill me now

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 # use the exec to create a manifest that kills a process killmenow
 
-exec { 'kill_killmenow_process':
-  command => 'pkill killmenow',
-  onlyif  => 'pgrep killmenow',
-  path    => '/usr/bin:/bin',
-  logoutput => true,
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
