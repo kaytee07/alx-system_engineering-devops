696ae58d927872f5199d0343b30c3192fa541215
kaytee07 <kofi.taylor07@gmail.com>
2023-07-24 21:00:35 +0000

    get data from api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+ returns information about his/her TODO list progress.
+"""
+import urllib.request
+import sys
+import json
+
+
+def get_data(employee_id):
+    url = 'https://jsonplaceholder.typicode.com/'
+    todos = url + 'todos'
+    users = url + f'users/{employee_id}'
+    num_of_task = 0
+    num_of_comp_task = 0
+    employee_name = ''
+    comp_task = []
+
+    try:
+        with urllib.request.urlopen(todos) as response:
+            raw_data = response.read()
+            info = json.loads(raw_data)
+            for todos in info:
+                if todos['userId'] == int(employee_id):
+                    num_of_task += 1
+                    if todos['completed'] is True:
+                        num_of_comp_task += 1
+                        comp_task.append(todos['title'])
+
+        with urllib.request.urlopen(users) as response:
+            raw_data = response.read()
+            info = json.loads(raw_data)
+            employee_name = info['name']
+        print('Employee {} is done with tasks({}/{}):'.
+              format(employee_name, num_of_comp_task, num_of_task))
+        for task in comp_task:
+            print(f'\t{task}')
+
+    except urllib.error.HTTPError as e:
+        print(f'Error: {e.code}, {e.reason}')
+    except urllib.error.URLError as e:
+        print(f'Error: {e.reason}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("pass the the employee id")
+    else:
+        get_data(sys.argv[1])
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+# Application Programming Interface(API)
+They are set of rules that allow software applications communicate with each other.
