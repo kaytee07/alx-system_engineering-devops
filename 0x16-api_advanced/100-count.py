940c93fd4e83028c3f389720bcc27e8f420728cd
kaytee07 <kofi.taylor07@gmail.com>
2023-08-08 13:17:19 +0000

    py 100

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+recursive function that queries the Reddit API, parses the title
+of all hot articles, and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces. Javascript should count as
+javascript, but java should not).
+"""
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    base_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Your User Agent"}
+
+    params = {"limit": 100, "count": count}
+
+    if after:
+        params["after"] = after
+
+    response = requests.get(base_url, headers=headers,
+                            params=params, allow_redirects=False)
+
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
