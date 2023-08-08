ab871241840e1d4fd862831e5d340022da2a183a
kaytee07 <kofi.taylor07@gmail.com>
2023-08-08 13:29:38 +0000

    add

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,9 +9,19 @@ import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Your User Agent"}
+    """Prints counts of given words found in hot posts of a given subreddit.
 
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     params = {
         "after": after,
         "count": count,
