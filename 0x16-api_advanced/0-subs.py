4a382c65a51ecc6a0a08016f93a2a816dd31efff
kaytee07 <kofi.taylor07@gmail.com>
2023-08-08 10:35:08 +0000

    check number of subs of a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given
+subreddit. If an invalid subreddit is given, the function should
+return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    elif response.status_code == 404:
+        return 0
+    else:
+        return 0
